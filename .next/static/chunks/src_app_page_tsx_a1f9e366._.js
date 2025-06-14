629cdd82520ae23071886a5c3d2ca5add1469973
mulkymalikuldhrs <mulkymalikuldhr@mail.com>
2025-06-14 23:32:08 +0700

    Add .next/static/chunks/src_app_page_tsx_a1f9e366._.js 1749918727468

---
+++ .next/static/chunks/src_app_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_26a7fd07._.js",
+  "static/chunks/src_2a1f7943._.js"
+],
+    source: "dynamic"
+});
